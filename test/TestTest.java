4e40da1e8720b2b808c8725e49ffad86068fc876
Steffen Trutz <trutz@oszimt.de>
2023-10-17 11:22:42 +0200

    Add tests

---
+++ test/TestTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+    /**
+     * Beispieltest, der nur testet ob Tests funktionieren
+     */
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
