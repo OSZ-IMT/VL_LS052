0e0272fc34662724c8e578c17b2143cc21e39fde
Steffen Trutz <trutz@oszimt.de>
2023-10-17 11:24:47 +0200

    Add tests

--- test/TestTest.java
+++ test/TestTest.java
@@ -2,10 +2,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Ignorieren Sie solange diese Datei, bis ihre Lehrkraft was dazu sagt.
+ */
 class TestTest {
 
     /**
-     * Beispieltest, der nur testet ob Tests funktionieren
+     * Ignorieren Sie solange diese Datei, bis ihre Lehrkraft was dazu sagt.
+     * Tests sind ein optionales Zusatzfeature, welches nicht in jeder Klasse genutzt wird.
+     * Beispieltest, der nur testet ob Tests funktionieren.
      */
     @Test
     void test() {
